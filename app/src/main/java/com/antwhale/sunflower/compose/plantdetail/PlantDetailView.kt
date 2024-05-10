01ee626334db842b63d4751ef4b490fa6c78eba6
Antwhale <olollolo339@naver.com>
2024-05-10 22:02:13 +0900

    PlantDetailView.kt ing...

--- app/src/main/java/com/antwhale/sunflower/compose/plantdetail/PlantDetailView.kt
+++ app/src/main/java/com/antwhale/sunflower/compose/plantdetail/PlantDetailView.kt
@@ -1,5 +1,13 @@
 package com.antwhale.sunflower.compose.plantdetail
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.Spring.StiffnessLow
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -9,9 +17,16 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.antwhale.sunflower.R
+import com.antwhale.sunflower.compose.Dimens
 import com.antwhale.sunflower.compose.utils.TextSnackbarContainer
 import com.antwhale.sunflower.data.Plant
 import com.antwhale.sunflower.viewmodels.PlantDetailViewModel
@@ -75,5 +90,81 @@ fun PlantDetails(
     }
     val transitionState =
         remember(plantScroller) { plantScroller.toolbarTransitionState }
+    val toolbarState = plantScroller.getToolbarState(LocalDensity.current)
 
+    //Transition that fades in/out the header with the image and the Toolbar
+    val transition = updateTransition(transitionState, label = "")
+
+    val toolbarAlpha = transition.animateFloat(
+        transitionSpec = { spring(stiffness = StiffnessLow) }, label = ""
+    ) { toolbarTransitionState ->
+        if(toolbarTransitionState == ToolbarState.HIDDEN) 0f else 1f
+    }
+
+    val contentAlpha = transition.animateFloat(
+        transitionSpec = { spring(stiffness = Spring.StiffnessLow) }, label = ""
+    ) { toolbarTransitionState ->
+        if(toolbarTransitionState == ToolbarState.HIDDEN) 1f else 0f
+    }
+
+    val toolbarHeightPx = with(LocalDensity.current) {
+        Dimens.PlantDetailAppBarHeight.roundToPx().toFloat()
+    }
+
+    val toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection{
+            override fun onPostScroll(
+                consumed: Offset,
+                available: Offset,
+                source: NestedScrollSource
+            ): Offset {
+                val delta = available.y
+                val newOffset = toolbarOffsetHeightPx.value + delta
+                toolbarOffsetHeightPx.value =
+                    newOffset
+                return Offset.Zero
+            }
+        }
+    }
+
+    Box(
+        modifier
+            .fillMaxSize()
+            //attach as a parent to the nested scroll system
+            .nestedScroll(nestedScrollConnection)
+    ) {
+        PlantDetailsContent(
+            scrollState = scrollState,
+            toolbarState = toolbarState,
+            onNamePosition = { newNamePosition ->
+                //Comparing to Float.MIN_VALUE as we are just interested on the original position of name on the screen.
+                if(plantScroller.namePosition == Float.MIN_VALUE) {
+                    plantScroller = plantScroller.copy(namePosition = newNamePosition)
+                }
+            },
+            plant = plant,
+            isPlanted = isPlanted,
+            hasValidUnsplashKey = hasValidUnsplashKey,
+            imageHeight = with(LocalDensity.current) {
+                val candidateHeight =
+                    Dimens.PlantDetailAppBarHeight + toolbarOffsetHeightPx.value.toDp()
+                maxOf(candidateHeight, 1.dp)
+            },
+            onFabClick = callbacks.onFabClick,
+            onGalleryClick = { callbacks.onGalleryClick(plant)},
+            contentAlpha = { contentAlpha.value }
+        )
+        PlantToolbar(
+            toolbarState, plant.name, callbacks,
+            toolbarAlpha = { toolbarAlpha.value },
+            contentAlpha = { contentAlpha.value }
+        )
+    }
 }
+
+@Composable
+private fun PlantDetailsContent(
+    scrollState: ScrollState,
+
+)
