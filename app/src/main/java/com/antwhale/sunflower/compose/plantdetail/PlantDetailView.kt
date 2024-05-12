52eff2df89ed7239563728d9d813f82c0e5da10b
Antwhale <olollolo339@naver.com>
2024-05-12 23:25:54 +0900

    Back up

--- app/src/main/java/com/antwhale/sunflower/compose/plantdetail/PlantDetailView.kt
+++ app/src/main/java/com/antwhale/sunflower/compose/plantdetail/PlantDetailView.kt
@@ -1,15 +1,38 @@
 package com.antwhale.sunflower.compose.plantdetail
 
+import android.text.Layout.Alignment
+import android.text.method.LinkMovementMethod
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.Spring.StiffnessLow
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -17,19 +40,32 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.core.text.HtmlCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.antwhale.sunflower.R
 import com.antwhale.sunflower.compose.Dimens
 import com.antwhale.sunflower.compose.utils.TextSnackbarContainer
+import com.antwhale.sunflower.compose.visible
 import com.antwhale.sunflower.data.Plant
+import com.antwhale.sunflower.databinding.ItemPlantDescriptionBinding
 import com.antwhale.sunflower.viewmodels.PlantDetailViewModel
+import org.openjdk.javax.tools.Tool
 import java.lang.reflect.Modifier
 
 data class PlantDetailsCallbacks(
@@ -164,7 +200,219 @@ fun PlantDetails(
 }
 
 @Composable
-private fun PlantDetailsContent(
-    scrollState: ScrollState,
+private fun PlantToolbar(
+    toolbarState: ToolbarState,
+    plantName: String,
+    callbacks: PlantDetailsCallbacks,
+    toolbarAlpha: () -> Float,
+    contentAlpha: () -> Float
+) {
+    val onShareClick = {
+        callbacks.onShareClick(plantName)
+    }
+    if(toolbarState.isShown) {
+        PlantDetailsToolbar(
+            plantName = plantName,
+            onBackClick = callbacks.onBackClick,
+            onShareClick = onShareClick,
+            modifier = androidx.compose.ui.Modifier.alpha(toolbarAlpha())
+        )
+    } else {
+        PlantHeaderActions(
+            onBackClick = callbacks.onBackClick,
+            onShareClick = onShareClick,
+            modifier = androidx.compose.ui.Modifier.alpha(contentAlpha())
+        )
+    }
+}
 
-)
+@Composable
+private fun PlantDetailsToolbar(
+    plantName: String,
+    onBackClick: () -> Unit,
+    onShareClick: () -> Unit,
+    modifier: androidx.compose.ui.Modifier
+) {
+    Surface {
+        TopAppBar(
+            modifier = modifier
+                .statusBarsPadding()
+                .background(color = MaterialTheme.colorScheme.surface),
+            title = {
+                Row {
+                    IconButton(
+                        onBackClick,
+                        androidx.compose.ui.Modifier.align(androidx.compose.ui.Alignment.CenterVertically)
+                    ) {
+                        Icon(
+                            Icons.Default.ArrowBack,
+                            contentDescription = stringResource(id = R.string.a11y_back)
+                        )
+                    }
+
+                    Text(
+                        text = plantName,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = androidx.compose.ui.Modifier
+                            .weight(1f)
+                            .fillMaxSize()
+                            .wrapContentSize(androidx.compose.ui.Alignment.Center)
+                    )
+
+                    val shareContentDescription = stringResource(R.string.menu_item_share_plant)
+
+                    IconButton(
+                        onShareClick,
+                        androidx.compose.ui.Modifier
+                            .align(androidx.compose.ui.Alignment.CenterVertically)
+                            .semantics { contentDescription = shareContentDescription }
+                    ) {
+                        Icon(
+                            Icons.Default.Share,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun PlantHeaderActions(
+    onBackClick: () -> Unit,
+    onShareClick: () -> Unit,
+    modifier: androidx.compose.ui.Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+            .padding(top = Dimens.ToolbarIconPadding),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        val iconModifier = androidx.compose.ui.Modifier
+            .sizeIn(
+                maxWidth = Dimens.ToolbarIconSize,
+                maxHeight = Dimens.ToolbarIconSize
+            )
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = CircleShape
+            )
+
+        IconButton(
+            onClick = onBackClick,
+            modifier = androidx.compose.ui.Modifier
+                .padding(start = Dimens.ToolbarIconPadding)
+                .then(iconModifier)
+        ) {
+            Icon(
+                Icons.Default.ArrowBack,
+                contentDescription = stringResource(id = R.string.a11y_back)
+            )
+
+            val shareContentDescription = stringResource(R.string.menu_item_share_plant)
+
+            IconButton(
+                onClick = onShareClick,
+                modifier = androidx.compose.ui.Modifier
+                    .padding(end = Dimens.ToolbarIconPadding)
+                    .then(iconModifier)
+                    .semantics {
+                        contentDescription = shareContentDescription
+                    }
+            ) {
+                Icon(
+                    Icons.Filled.Share,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+
+
+}
+
+@Composable
+private fun PlantInformation(
+    name: String,
+    wateringInterval: Int,
+    description: String,
+    hasValidUnsplashKey: Boolean,
+    onNamePosition: (Float) -> Unit,
+    toolbarState: ToolbarState,
+    onGalleryClick: () -> Unit,
+    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier
+) {
+    Column(modifier = modifier.padding(Dimens.PaddingLarge)) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.displaySmall,
+            modifier = androidx.compose.ui.Modifier
+                .padding(
+                    start = Dimens.PaddingSmall,
+                    end = Dimens.PaddingSmall,
+                    bottom = Dimens.PaddingNormal
+                )
+                .align(androidx.compose.ui.Alignment.CenterHorizontally)
+                .onGloballyPositioned { onNamePosition(it.positionInWindow().y) }
+                .visible { toolbarState == ToolbarState.HIDDEN }
+        )
+
+        Box(
+            androidx.compose.ui.Modifier
+                .align(androidx.compose.ui.Alignment.CenterHorizontally)
+                .padding(
+                    start = Dimens.PaddingSmall,
+                    end = Dimens.PaddingSmall,
+                    bottom = Dimens.PaddingNormal
+                )
+        ) {
+            Column(androidx.compose.ui.Modifier.fillMaxWidth()) {
+                Text(
+                    text = stringResource(id = R.string.watering_needs_prefix),
+                    fontWeight = FontWeight.Bold,
+                    modifier = androidx.compose.ui.Modifier
+                        .padding(horizontal = Dimens.PaddingSmall)
+                        .align(androidx.compose.ui.Alignment.CenterHorizontally)
+                )
+
+                val wateringIntervalText = pluralStringResource(
+                    R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
+                )
+
+                Text(
+                    text = wateringIntervalText,
+                    modifier = androidx.compose.ui.Modifier
+                        .align(androidx.compose.ui.Alignment.CenterHorizontally)
+                )
+            }
+
+            if(hasValidUnsplashKey) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_photo_library),
+                    contentDescription = "Gallery Icon",
+                    androidx.compose.ui.Modifier
+                        .clickable { onGalleryClick() }
+                        .align(androidx.compose.ui.Alignment.CenterEnd)
+                )
+            }
+        }
+
+        PlantDescription(description)
+    }
+}
+
+@Composable
+private fun PlantDescription(description: String) {
+    //This remains using AndroidViewBinding because this feature is not in Compose yet
+    AndroidViewBinding(ItemPlantDescriptionBinding::inflate) {
+        plantDescription.text = HtmlCompat.fromHtml(
+            description,
+            HtmlCompat.FROM_HTML_MODE_COMPACT
+        )
+        plantDescription.movementMethod = LinkMovementMethod.getInstance()
+        plantDescription.linksClickable = true
+    }
+}
